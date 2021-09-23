44a26e1828c1157f3020e464ac88f14bc26bd7e5
VitaliyKorolev <ghost050699@mail.ru>
2021-09-23 16:16:45 +0300

    Update Program.cs

--- 01-csharp-basics/CSharpBasics/Task5/Program.cs
+++ 01-csharp-basics/CSharpBasics/Task5/Program.cs
@@ -6,7 +6,16 @@ namespace Task5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = 0;
+            for(int i=1; i<1000; i++)
+            {
+                if(i % 3==0 || i % 5 == 0)
+                {
+                    count = count + i;
+                }
+
+            }
+            Console.WriteLine(count);
         }
     }
 }
