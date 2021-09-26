9e86c5a901f5626b8dc919b76934c0d14ab53962
VitaliyKorolev <ghost050699@mail.ru>
2021-09-26 14:58:00 +0300

    Second Homework

---
+++ 02-arrays-and-methods/ArraysAndMethods/Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = GenerateArray();
+            PrintArray(A);
+            Console.WriteLine(GetSumOfNonNegativeElements(A));
+        }
+        static int[] GenerateArray()
+        {
+            int[] arr = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(-10,10);
+            }
+            return arr;
+        }
+        static void PrintArray(int[] arr)
+        {
+            foreach (int x in arr)
+            {
+                Console.WriteLine(x);
+            }
+        }
+        static int GetSumOfNonNegativeElements( int[] mas)
+        {
+            int sum = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] > 0)
+                {
+                    sum = sum + mas[i];
+                }
+                
+            }
+            return sum;
+
+        }
+    }
+}
---
+++ 02-arrays-and-methods/ArraysAndMethods/Task4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = GenerateArray();
+            PrintArray(A);
+            Console.WriteLine(GetSumOfElementsOnEvenPositions(A));
+        }
+
+        static int[,] GenerateArray()
+        {
+            int[,] arr = new int[2, 5];
+            Random r = new Random();
+            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+                {
+                    arr[i, j] = r.Next(-10, 10);  
+                }
+
+            }
+            return arr;
+        }
+        static void PrintArray(int[,] arr)
+        {
+            foreach (int x in arr)
+            {
+                Console.Write($"{x} ");
+            }
+            Console.WriteLine();
+        }
+        static int GetSumOfElementsOnEvenPositions( int[,] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+                {
+                    
+                        if ((i+j) % 2 == 0)
+                        {
+                            sum = sum +arr[i, j] ;
+                        }
+                    
+                }
+
+            }
+            return sum;
+
+        }
+    }
+}
