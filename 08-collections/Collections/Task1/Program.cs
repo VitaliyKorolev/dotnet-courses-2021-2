bc2c1ad34082e0d802b425eff89b6946c4c763be
VitaliyKorolev <ghost050699@mail.ru>
2021-10-15 12:51:05 +0300

    3

--- 08-collections/Collections/Task1/Program.cs
+++ 08-collections/Collections/Task1/Program.cs
@@ -7,7 +7,7 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            List<int> list1 = new List<int> { 1, 2, 3, 4};
+            List<int> list1 = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
             LinkedList<string> list2 = new LinkedList<string>();
             list2.AddLast("Vanya");
             list2.AddLast("Bogdan");
@@ -22,27 +22,20 @@ namespace Task1
         }
         static void RemoveEachSecondItem <T> ( ICollection<T> collection)  
         {
-            while (collection.Count != 1)
+            
+            while ( collection.Count!=1)
             {
-                List<T> delete = new List<T>();
                 var a = collection.GetEnumerator();
-                int count = 0;
-                while (a.MoveNext())
-                {
-                    count++;
-                    
-                    if (count % 2 == 0)
-                    {
-                        delete.Add(a.Current);
-                    }
-                }
-                foreach(T del in delete)
-                {
-                    collection.Remove(del);
-                }
-
+                a.MoveNext();
+                var temp = a.Current;
+                collection.Remove(a.Current);
+                collection.Add(temp);
+                var b = collection.GetEnumerator();
+                b.MoveNext();
+                collection.Remove(b.Current);
             }
             
+
         }
         static void Print<T>(ICollection<T> collection)
         {
--- 08-collections/Collections/Task3/Program.cs
+++ 08-collections/Collections/Task3/Program.cs
@@ -8,15 +8,20 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            string s = "Hello my name is Name????787585 name";
+            string s = "Hello/ my. name is,  Name????787585 name is";
             var words = CountWords(s);
+            foreach(var el in words)
+            {
+                Console.WriteLine(el);
+            }
         }
         public static Dictionary<string, int>  CountWords(string s)
         {
             s= s.ToLower();
             Regex regex = new Regex(@"[^a-zA-Z]", RegexOptions.IgnoreCase);
             var set = new HashSet<string>();
-           
+           /// string[] resultwords1 = Regex.Split(s , "[^a-zA-Z]" );
+            
             Dictionary<string, int> words = new Dictionary<string, int>();
             string result = regex.Replace(s, " ");
             string[] resultwords = result.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
